11acb5772cfed375a67d4192d391e9b2c050e95d
Lukasz Pawelczyk <havner@gmail.com>
2026-03-06 01:32:05 +0100

    Replace simplelog with env_logger

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,7 @@ impl global_events::GlobalEventHandler for GlobalEventHandler {}
 
 #[tokio::main]
 async fn main() -> OpenActionResult<()> {
-	{
-		use simplelog::*;
-		if let Err(error) = TermLogger::init(
-			LevelFilter::Debug,
-			Config::default(),
-			TerminalMode::Stdout,
-			ColorChoice::Never,
-		) {
-			eprintln!("Logger initialization failed: {}", error);
-		}
-	}
+	env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
 	global_events::set_global_event_handler(&GlobalEventHandler);
 	register_action(uinput_simulation::InputSimulationAction).await;
