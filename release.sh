dc7020c2e18ab76b5b28220640e0e729d389efa6
Lukasz Pawelczyk <havner@gmail.com>
2026-03-07 20:23:41 +0100

    Add aarch64 cross-compilation
    Hopefully this'll work, I have no means to test it.

--- release.sh
+++ release.sh
@@ -2,8 +2,17 @@
 
 PLUGIN="com.havner.toolkit.sdPlugin"
 TARGET="x86_64-unknown-linux-gnu"
+TARGETS=("x86_64-unknown-linux-gnu" "aarch64-unknown-linux-gnu")
+INSTALLED_TARGETS="$(rustup target list --installed)"
 
 rm -rf "$PLUGIN"                                                       || exit 1
 mkdir "$PLUGIN"                                                        || exit 1
 cp -r assets/* "$PLUGIN"/                                              || exit 1
-cargo install --target "$TARGET" --path . --root "$PLUGIN/$TARGET"     || exit 1
+
+for TARGET in "${TARGETS[@]}"; do
+	if grep -qx "$TARGET" <<<"$INSTALLED_TARGETS"; then
+		cargo install --target "$TARGET" --path . --root "$PLUGIN/$TARGET"
+	else
+		echo "Skipping target '$TARGET' (not installed in rustup)."
+	fi
+done
